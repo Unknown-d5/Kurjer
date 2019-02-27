d3ffe6c23f5bd86d939ba0dc3b9466f23fcaed49
Adrian Krawczyk <adrian.krawczyk1@outlook.com>
2019-02-27 18:25:29 +0100

    Dodaj pliki projektów.

---
+++ Kurjerzy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kurjerzy.MvcApplication" Language="C#" %>
---
+++ Kurjerzy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kurjerzy.Startup))]
+namespace Kurjerzy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
